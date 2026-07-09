88ef48ac4bfef8afccab86395b9deb3713c2c719
shashi <shashiyadav6815@gmail.com>
2026-07-10 02:20:43 +0530

    socketio url update

--- src/component/socket.js
+++ src/component/socket.js
@@ -1,5 +1,5 @@
 import {io} from 'socket.io-client'
-export const socket = io("https://vercelback-production.up.railway.app/",{
+export const socket = io("https://vercelback-production.up.railway.app",{
      autoConnect : false,
 }
    
