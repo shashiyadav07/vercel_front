2ae140b6c8243d8e2abe5cbcd6b851cccefa94c5
shashi <shashiyadav6815@gmail.com>
2026-07-09 23:14:27 +0530

    adding frontend

--- src/component/Signup.jsx
+++ src/component/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./Signup.css";
+import "./signup.css";
 
 function Signup() {
   const navigate = useNavigate();
